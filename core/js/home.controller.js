e5c3d64b163d2a5b89b7a173463a1593cbccf682
Heriberto Basulto <basulto93@live.com>
2016-08-12 08:54:49 -0700

    first push, still having issues with styling

---
+++ core/js/home.controller.js
@@ -0,0 +1,13 @@
+angular.module('app').controller('ToDoListController', function($scope){
+	//$scope.taskItem = "";
+	//$scope.taskPriority = "";
+	$scope.submitTask = submitTask;
+	$scope.taskList = [];
+	$scope.toDo = {};
+
+	function submitTask(){
+		$scope.taskList.push(angular.copy($scope.toDo));
+		console.log($scope.toDo);
+		$scope.toDo.name = "";
+	}
+}); 
